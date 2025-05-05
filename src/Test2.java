681f40185d74f7b16ebc4ec4e1dc0d6868fe81f0
Thamidu <t4365732@gmail.com>
2025-05-05 12:36:08 +0530

    Done

---
+++ src/Test2.java
@@ -0,0 +1,28 @@
+public class Test2 {
+    private int top;
+    private  int[] arr;
+
+    public Test2(int maxSize){
+        top = -1;
+        arr = new int[maxSize];
+    }
+
+    public void test(int number){
+        arr[++top] = number;
+
+
+    }
+    public void getTop(){
+        System.out.println("Top value " + top);
+    }
+
+    public static void main(String[] args) {
+        Test2 t = new Test2(4);
+        t.test(1);
+        t.test(2);
+        t.test(3);
+
+        t.getTop();
+    }
+
+}
